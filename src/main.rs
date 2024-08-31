c0b17ea81f8a715f9dff3a22b3958808cbef9dbe
Johnathan W <12262567+j7nw4r@users.noreply.github.com>
2024-08-31 16:06:57 -0400

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 use serde_json;
 use std::env;
-
 // Available if you need it!
 // use serde_bencode
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+    let mut encoded_chars = encoded_value.chars();
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+    let Some(encoded_char) = encoded_chars.next() else {
+        panic!("Unhandled encoded value: {}", encoded_value)
+    };
+    
+    if encoded_char.is_digit(10) {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         return serde_json::Value::String(string.to_string());
+    } else if encoded_char.eq_ignore_ascii_case(&'i')  {
+        let Some(suffix) = encoded_value.strip_prefix("i") else {
+            panic!("Unhandled encoded value: {}", encoded_value)
+        };
+        let parts = suffix.split("e").map(|x| x.to_owned()).collect::<Vec<String>>();
+        let Some(num) = parts.first() else {
+            panic!("Unhandled encoded value: {}", encoded_value)
+        };
+        let Ok(parsed_num) = num.parse::<u32>() else {
+            panic!("Unhandled encoded value: {}", encoded_value)
+        };
+        return serde_json::Value::Number(parsed_num.into());
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
